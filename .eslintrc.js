5efab59f96ee4ff4bb6d71de14a480584acb151f
Phillippe Rocha <phillipperocha@gmail.com>
2019-12-30 22:22:45 -0300

    Produto duplicado

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', 'js'] }],
     'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
     'no-console': ['error', { allow: ['tron'] }],
   },
 };
