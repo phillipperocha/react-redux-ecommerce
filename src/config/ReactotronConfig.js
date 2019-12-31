f3e3b566913e620b5a6f20ba36ea19670b2c169a
Phillippe Rocha <phillipperocha@gmail.com>
2019-12-30 21:51:24 -0300

    Reactotron + redux

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,12 @@
+import ReactoTron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+
+if (process.env.NODE_ENV === 'development') {
+  const tron = ReactoTron.configure()
+    .use(reactotronRedux())
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
